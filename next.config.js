c0bb8ee6dd9c14288e00f2964b58463b134fa2c2
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2022-10-30 21:53:05 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: false,
+	experimental: {
+		appDir: true,
+		newNextLinkBehavior: true,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
