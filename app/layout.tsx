0e0fed6e41c0a0c828e7699345a5b4bbd12636d4
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2022-11-01 21:23:59 +0100

    added tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,23 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html>
-      <head></head>
-      <body>{children}</body>
-    </html>
-  )
+import "tailwindcss/tailwind.css";
+import NavLink from "./nav-link";
+
+export default function RootLayout({ children }: any) {
+	return (
+		<html className='bg-gray-900 text-gray-100 antialiased'>
+			<head>
+				<title>Nested layouts by example</title>
+			</head>
+
+			<body>
+				<header className='border-b p-4'>
+					<nav className='space-x-4'>
+						<NavLink href='/'>Home</NavLink>
+						<NavLink href='/movies'>Movies</NavLink>
+					</nav>
+				</header>
+
+				<div className='p-4'>{children}</div>
+			</body>
+		</html>
+	);
 }
